15d24c3d20908f89bb6b22c42faa212a4b40a35c
Xinwei <zxw018018@hotmail.com>
2017-12-13 07:07:00 -0500

    almost done

---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mysql = require('mysql'); 
+
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+var userID = [-1,-1,-1,-1,-1,-1];
+
+var connection = mysql.createConnection({  
+    host: "127.0.0.1",   
+    user: "root",   
+    password: "123456",  
+    database: "expedia"  
+    });    
+
+    var userID = [-1,-1,-1,-1,-1,-1];
+
+app.get("/", function(req, res){
+    res.render("home", {userID: userID});
+});
+
+
+app.post("/search", function(req, res){
+    userID.push(req.body.newUserID);
+    userID.shift();
+
+    get_info(userID[5], function(result){   
+        
+        userID.push(result[0]);
+        userID.shift();
+        userID.push(result[1]);
+        userID.shift();
+        userID.push(result[2]);
+        userID.shift();
+        userID.push(result[3]);
+        userID.shift();
+        userID.push(result[4]);
+        userID.shift();
+        res.render("search", {userID: userID});
+    });   
+
+    
+});
+
+app.get("*", function(req, res){
+    res.send("wrong website");
+});
+
+app.listen(3000, function(){
+    console.log("running");
+});
+
+function get_info(userID, callback){    
+    var sql = "select *" + " from expedia where id = "+userID;   
+    connection.query(sql, function(err, results){    
+          if (err){    
+            throw err; 
+          }     
+          var cluster1 = results[0].cluster1;   
+          var cluster2 = results[0].cluster2;    
+          var cluster3 = results[0].cluster3;    
+          var cluster4 = results[0].cluster4;   
+          var cluster5 = results[0].cluster5;    
+          var result = [cluster1, cluster2, cluster3, cluster4, cluster5];   
+          return callback(result);   
+  });  
+}    
--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "127.0.0.1:3306",
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
   user: "root",
-  password: "123456"
+  password: "123456",
+  database: "expedia"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+var userID = '1';
+
+var output = new Array(5);
+
+get_info(userID, function(result){
+    console.log(result);
 });
+
+
+function get_info(userID, callback){
+          var sql = "select *" + " from expedia where id = "+userID;
+          connection.query(sql, function(err, results){
+                if (err){ 
+                  throw err;
+                }
+                var cluster1 = results[0].cluster1;
+                var cluster2 = results[0].cluster2;
+                var cluster3 = results[0].cluster3;
+                var cluster4 = results[0].cluster4;
+                var cluster5 = results[0].cluster5;
+                var result = [cluster1, cluster2, cluster3, cluster4, cluster5];
+                return callback(result);
+        });
+    }
+    
+
+    
